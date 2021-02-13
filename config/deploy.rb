b7f95bd9da41105495254246abd82913ec68b80b
Dipak Jadam <dipakjadam@gmail.com>
2021-02-14 00:05:55 +0530

    revert test commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,7 +47,7 @@ set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/sys
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-set :branch, 'master'
+set :branch, 'main'
 
 
 set :scm,           :git
