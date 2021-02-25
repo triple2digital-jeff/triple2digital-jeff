664930733f89fac1f9ea7defd331c6ac3ff68d36
Dipak Jadam <dipakjadam@gmail.com>
2021-02-25 09:58:48 +0530

    Added stripe key

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,3 +1,3 @@
 require 'stripe'
 # Stripe.api_key = 'sk_test_dkiZ18u1tjA6sejpeEsphufy004PoASDqW'
-Stripe.api_key = 'sk_test_lQTRtqfThKChr4C1tKJD7FYx00jeGq5nCB'
+Stripe.api_key = Rails.application.secrets.stript_api_key
